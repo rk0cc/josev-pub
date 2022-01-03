eac421d16dd3243d8d9b06bce88a9b8b7181ba67
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-03 18:55:17 +0800

    Add one more test task

--- src/test/java/xyz/rk0cc/josev/constraint/pub/PubSemVerConstraintTest.java
+++ src/test/java/xyz/rk0cc/josev/constraint/pub/PubSemVerConstraintTest.java
@@ -1,6 +1,7 @@
 package xyz.rk0cc.josev.constraint.pub;
 
 import org.junit.jupiter.api.*;
+import xyz.rk0cc.josev.constraint.SemVerConstraint;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +18,12 @@ final class PubSemVerConstraintTest {
         assertDoesNotThrow(() -> PubSemVerConstraint.parse(">=1.0.0"));
         assertDoesNotThrow(() -> PubSemVerConstraint.parse("<=1.0.0"));
         assertDoesNotThrow(() -> PubSemVerConstraint.parse(null));
+    }
+
+    @DisplayName("Test parsing from parent class")
+    @Order(2)
+    @Test
+    void testParentParser() {
+        assertDoesNotThrow(() -> SemVerConstraint.parse(PubSemVerConstraint.class, "^1.0.0"));
     }
 }
