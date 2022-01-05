b2a59f5e88231252ef13ee88c94787ec28c82e71
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-05 11:48:04 +0800

    Add another parser and document

--- src/main/java/xyz/rk0cc/josev/constraint/pub/PubSemVerConstraint.java
+++ src/main/java/xyz/rk0cc/josev/constraint/pub/PubSemVerConstraint.java
@@ -9,8 +9,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
+/**
+ * A fully implemented {@link SemVerConstraint} for pub package (Dart/Flutter).
+ *
+ * @since 1.0.0
+ *
+ * @see PubConstraintPattern
+ * @see <a href="https://dart.dev/tools/pub/dependencies#version-constraints">Version constraint for pub dependency</a>.
+ */
 @SemVerConstraintParser
 public final class PubSemVerConstraint extends SemVerConstraint<PubConstraintPattern> {
+    /**
+     * Create new data of pub package dependency's version constraint.
+     *
+     * @param constraintPattern Pattern uses in this constraint
+     * @param rawConstraint A {@link String} of user input, can be <code>null</code>
+     * @param start A {@link SemVerRangeNode node} contains which minimum version can be allowed.
+     * @param end A {@link SemVerRangeNode node} contains which maximum version can be allowed.
+     */
     private PubSemVerConstraint(
             @Nonnull PubConstraintPattern constraintPattern,
             @Nullable String rawConstraint,
@@ -20,6 +36,15 @@ public final class PubSemVerConstraint extends SemVerConstraint<PubConstraintPat
         super(constraintPattern, rawConstraint, start, end);
     }
 
+    /**
+     * Parse <code>versionConstraint</code> on pub package dependency.
+     * <br/>
+     * This method can be invoked by {@link SemVerConstraint#parse(Class, String)}.
+     *
+     * @param versionConstraint Version constraint {@link String} of pub package dependency.
+     *
+     * @return A {@link PubSemVerConstraint} of <code>versionConstraint</code>.
+     */
     @Nonnull
     public static PubSemVerConstraint parse(@Nullable String versionConstraint) {
         List<PubConstraintPattern> constraintPatterns = Arrays.stream(PubConstraintPattern.values())
@@ -28,9 +53,29 @@ public final class PubSemVerConstraint extends SemVerConstraint<PubConstraintPat
 
         assert constraintPatterns.size() == 1;
 
-        final PubConstraintPattern pattern = constraintPatterns.get(0);
+        return parse(constraintPatterns.get(0), versionConstraint);
+    }
 
+    /**
+     * Parse <code>versionConstraint</code> on pub package dependency and provides which {@link PubConstraintPattern} is
+     * preferred.
+     *
+     * @param pattern Preferred {@link PubConstraintPattern} is used in {@link PubSemVerConstraint}.
+     * @param versionConstraint Version constraint {@link String} of pub package dependency.
+     *
+     * @return A {@link PubSemVerConstraint} of <code>versionConstraint</code>.
+     *
+     * @throws IllegalArgumentException If provided {@link PubConstraintPattern} is invalid for
+     *                                  <code>versionConstraint</code>.
+     */
+    @Nonnull
+    public static PubSemVerConstraint parse(
+            @Nonnull PubConstraintPattern pattern,
+            @Nullable String versionConstraint
+    ) {
         try {
+            assert pattern.isValidConstraintMethods(versionConstraint);
+
             switch (pattern) {
                 case ANY:
                     return new PubSemVerConstraint(pattern, versionConstraint, null, null);
@@ -91,8 +136,8 @@ public final class PubSemVerConstraint extends SemVerConstraint<PubConstraintPat
                 default:
                     throw new IllegalArgumentException("Pattern " + pattern + " is undefined.");
             }
-        } catch (NonStandardSemVerException e) {
-            throw new AssertionError(e);
+        } catch (NonStandardSemVerException | AssertionError e) {
+            throw new IllegalArgumentException(e);
         }
     }
 }
