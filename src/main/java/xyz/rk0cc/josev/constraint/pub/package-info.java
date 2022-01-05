a9d1c1944ffacd3f405df6c18ef98d9fab48f424
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-05 12:39:19 +0800

    Document package info

--- src/main/java/xyz/rk0cc/josev/constraint/pub/package-info.java
+++ src/main/java/xyz/rk0cc/josev/constraint/pub/package-info.java
@@ -1,1 +1,12 @@
+/**
+ * Extension package of {@link xyz.rk0cc.josev.constraint.SemVerConstraint} for
+ * <a href="https://dart.dev/tools/pub/dependencies">pub package dependency</a>.
+ * <br/>
+ * <code>pub</code> is a package dependency management system for <a href="https://dart.dev/">Dart</a> programming
+ * language (including <a href="https://flutter.dev/">Flutter</a>, one of the well known Dart implementation of
+ * cross-platform GUI interface) which mainly uses for developing cross-platform client-base program including Android,
+ * iOS, Fuchsia, Windows, macOS and Linux or web-based Javascript program.
+ *
+ * @since 1.0.0
+ */
 package xyz.rk0cc.josev.constraint.pub;
